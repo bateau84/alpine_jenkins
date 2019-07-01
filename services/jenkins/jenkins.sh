6de5e3c030a76d1230636bba2e3ab1d126733833
Mats Bergmann <bateau@sea-shell.org>
2019-07-01 13:56:27 +0200

    deps added

--- services/jenkins/jenkins.sh
+++ services/jenkins/jenkins.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 ## Add some tools
-apk --no-cache add curl git openssh-client jq fontconfig font-xfree86-type1 
+apk --no-cache add curl git openssh-client jq fontconfig font-xfree86-type1 nss nss-dev 
 
 # runit
 mkdir -p /etc/service/jenkins
